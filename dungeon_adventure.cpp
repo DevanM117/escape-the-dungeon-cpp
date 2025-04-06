61a019d142e3919e2cd574c9fc4875324b823a42
DevanM117 <devanmarion8@gmail.com>
2025-04-06 08:28:42 +0000

    Added Include statements

--- dungeon_adventure.cpp
+++ dungeon_adventure.cpp
@@ -1,1 +1,19 @@
+//************************************************************************
+//Author.....: Devan Marion
+//Assignment: Escape the Dungeon
+//Despcription: This program provides a simple dungeon adventure game 
+//************************************************************************ 
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+// Clears invalid input from the input buffer
+void clearInput() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
 
